14068fb725dcc97f5d3257916f9782a8b6f8519d
Jordan Hopfner <nickelfault@gmail.com>
2015-05-19 10:34:24 -0600

    Added request for "Ottawa Citizen" (http://ottawacitizen.com/) and updated permissions so there is no need for constant update prompting.

--- js/bypass.js
+++ js/bypass.js
@@ -1,9 +1,28 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
         return {
-            cancel: details.url.indexOf("://s.ppjol.net/") != -1 || details.url.indexOf("paywall.js") != -1 || details.url.indexOf("paywall.min.js") != -1
+            cancel: details.url.indexOf("ppjol.net/") != -1 || details.url.indexOf("paywall.js") != -1 || details.url.indexOf("paywall.min.js") != -1
         };
     },
-    {urls: ["<all_urls>"]},
+    {urls: [
+		"*://*.edmontonjournal.com/*",
+        "*://*.montrealgazette.com/*",
+        "*://*.leaderpost.com/*",
+        "*://*.thestarphoenix.com/*",
+        "*://*.theprovince.com/*",
+        "*://*.vancouversun.com/*",
+        "*://*.timescolonist.com/*",
+        "*://*.windsorstar.com/*",
+        "*://*.nationalpost.com/*",
+        "*://*.financialpost.com/*",
+        "*://*.edmontonsun.com/*",
+        "*://*.calgarysun.com/*",
+        "*://*.ottawasun.com/*",
+        "*://*.torontosun.com/*",
+        "*://*.winnipegsun.com/*",
+        "*://*.thewesternstar.com/*",
+        "*://*.ppjol.net/*",
+        "*://*.ottawacitizen.com/*"
+	]},
     ["blocking"]
 );
