5d8ac6719161e1a21bccad8ec1cc621059d59105
Jordan <nickelfault@gmail.com>
2016-12-21 12:26:05 -0700

    Fix for Winnipeg Free Press

--- js/wfp.js
+++ js/wfp.js
@@ -4,3 +4,11 @@ $(window).load(function() {
 	$('html').css({overflow: 'visible', marginRight: 0});
 	$('.mfp-bg, .mfp-wrap, #cTempStyles').remove();
 });
+
+$(function() {
+	var article = $('.text-wrapper').html();
+	
+	$(window).load(function() {
+		$('.text-wrapper').html(article);
+	});
+});
